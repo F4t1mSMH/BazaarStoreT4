1d089d952ee11f9369ba48008d72b859b411a370
fatima <fatima0alhammam@gmail.com>
2025-11-09 16:24:53 +0300

    Add ProductsPage for Manger

--- src/test/java/com/bazaarstores/pages/AddProductPage.java
+++ src/test/java/com/bazaarstores/pages/AddProductPage.java
@@ -1,8 +1,6 @@
-package com.bazaarstores.pages.MangerPages;
+package com.bazaarstores.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class AddProductPage extends ProductsPage {
     private final By productNameInput = By.name("name");
@@ -11,29 +9,34 @@ public class AddProductPage extends ProductsPage {
     private final By productSKUInput = By.name("sku");
     private final By submitButton = By.xpath("//button[@type='submit']");
     private final By successMessage = By.xpath("//*[@class='toast-message']");
-    private final By errorMessage = By.cssSelector("//section[@id='multiple-column-form']//div[contains(@class, 'alert')]");
-
+    private final By errorMessage = By.xpath("//div[contains(@class, 'alert') or contains(@class, 'error')]");
 
     public AddProductPage enterProductName(String name) {
+        sendKeys(productNameInput, name);
         return this;
     }
 
     public AddProductPage enterProductPrice(int price) {
+        sendKeys(productPriceInput, String.valueOf(price));
         return this;
     }
 
     public AddProductPage enterProductStock(int stock) {
+        sendKeys(productStockInput, String.valueOf(stock));
         return this;
     }
 
     public AddProductPage enterProductSKU(int sku) {
+        sendKeys(productSKUInput, String.valueOf(sku));
         return this;
     }
 
     public AddProductPage clickSubmitButton() {
-        click(submitButton);
-        return new AddProductPage();
+        clickWithJS(submitButton);
+        return this;
     }
+
+
     public boolean isErrorMessageDisplayed() {
         return isDisplayed(errorMessage);
     }
