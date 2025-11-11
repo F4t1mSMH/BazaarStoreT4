51b3cf63a5817f089f7bc6bc233cba3a1cd7308e
fatima <fatima0alhammam@gmail.com>
2025-11-11 12:41:22 +0300

    Manger Pages

--- src/test/java/com/bazaarstores/pages/AddProductPage.java
+++ src/test/java/com/bazaarstores/pages/AddProductPage.java
@@ -1,42 +1,69 @@
 package com.bazaarstores.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement; // ⭐ Required Import
+import org.openqa.selenium.support.ui.Select; // ⭐ Required Import
 
-public class AddProductPage extends ProductsPage {
+import static org.codehaus.groovy.tools.shell.util.Preferences.clear;
+
+public class AddProductPage extends ProductsPage{
     private final By productNameInput = By.name("name");
     private final By productPriceInput = By.name("price");
     private final By productStockInput = By.name("stock");
     private final By productSKUInput = By.name("sku");
+    private final By categoryDropdown = By.name("category_id");
     private final By submitButton = By.xpath("//button[@type='submit']");
     private final By successMessage = By.xpath("//*[@class='toast-message']");
-    private final By errorMessage = By.xpath("//div[contains(@class, 'alert') or contains(@class, 'error')]");
+    private final By errorMessage = By.xpath("//section[@id='multiple-column-form']//div[contains(@class, 'alert')]");
+
+    public AddProductPage selectDropdownByValue(By locator, int value) {
+        WebElement element = findElement(locator);
+        Select select = new Select(element);
+        select.selectByValue(String.valueOf(value));
+        return this;
+    }
+
+    public AddProductPage selectProductCategory(int categoryId) {
+        return selectDropdownByValue(categoryDropdown, categoryId);
+    }
+
+    public AddProductPage enterAllProductDetails(String name, int price, int stock, int sku, int categoryId) {
+        enterProductName(name);
+        enterProductPrice(price);
+        enterProductStock(stock);
+        enterProductSKU(sku);
+        selectProductCategory(categoryId);
+        return this;
+    }
 
     public AddProductPage enterProductName(String name) {
+        clear(productNameInput);
         sendKeys(productNameInput, name);
         return this;
     }
 
     public AddProductPage enterProductPrice(int price) {
+        clear(productPriceInput);
         sendKeys(productPriceInput, String.valueOf(price));
         return this;
     }
 
     public AddProductPage enterProductStock(int stock) {
+        clear(productStockInput);
         sendKeys(productStockInput, String.valueOf(stock));
         return this;
     }
 
     public AddProductPage enterProductSKU(int sku) {
+        clear(productSKUInput);
         sendKeys(productSKUInput, String.valueOf(sku));
         return this;
     }
 
     public AddProductPage clickSubmitButton() {
         clickWithJS(submitButton);
-        return this;
+        return new AddProductPage();
     }
-
-
     public boolean isErrorMessageDisplayed() {
         return isDisplayed(errorMessage);
     }
@@ -48,7 +75,6 @@ public class AddProductPage extends ProductsPage {
     public boolean isSuccessMessageDisplayed() {
         return isDisplayed(successMessage);
     }
-
     public String getSuccessMessageText() {
         return getText(successMessage);
     }
--- src/test/java/com/bazaarstores/pages/ProductsPage.java
+++ src/test/java/com/bazaarstores/pages/ProductsPage.java
@@ -4,22 +4,41 @@ import org.openqa.selenium.By;
 
 public class ProductsPage extends DashboardPage {
     private By addProductButton = By.cssSelector("a[href*='product/create']");
-    private By editButton = By.xpath("//*[@id=\"table-bordered\"]/div/div/div/div/table/tbody/tr[6]/td[6]/button[1]");
-    private By deleteButton = By.xpath("//*[@id=\"table-bordered\"]/div/div/div/div/table/tbody/tr[6]/td[6]/button[2]");
-    private final String PRODUCT_TEMPLATE = "//td[contains(text(), '{0}')]";
+    private final String productTable = "//td[contains(text(), '{0}')]";
+    private final String editButton = "//tr[td[contains(text(), '{0}')]]//button[1]";
+    private final String deleteButton = "";
+    private final String confirmDelete = "//tr[td[contains(text(), '{0}')]]//button[2]";
+    private final String cancelDelete = "//tr[td[contains(text(), '{0}')]]//button[2]";
+
     public void clickAddProductButton() {
         click(addProductButton);
     }
+
+    public void clickEditButtonByName(String productName) {
+        By editLocator = By.xpath(editButton.replace("{0}", productName));
+        click(editLocator);
+    }
+
+    public void clickDeleteButtonByName(String productName) {
+        By deleteLocator = By.xpath(deleteButton.replace("{0}", productName));
+        click(deleteLocator);
+    }
+
     public boolean isProductDisplayed(String productName) {
-        By productLocator = By.xpath(PRODUCT_TEMPLATE.replace("{0}", productName));
+        By productLocator = By.xpath(productTable.replace("{0}", productName));
 
         return isDisplayed(productLocator);
     }
-    public void clickEditButton() {
+
+    public void clickConfirmDeleteButton() {
+        click(By.xpath(confirmDelete));
     }
 
-    public void clickDeleteButton() {
-
+    public void clickCancelDeleteButton() {
+        click(By.xpath(cancelDelete));
     }
 
+    public void searchProduct(String productToVerifyName) {
+
+    }
 }
